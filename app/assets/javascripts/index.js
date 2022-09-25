5f4d0203bacfca1843a565be301411aa2f7a5c47
shogo1231 <shogo123198@gmail.com>
2022-09-25 19:48:13 +0900

    fix　カード要素内のクリック処理修正
    カード内の画像部分クリックでサイト移動、ボタンクリックで詳細ページに遷移するように修正。

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,21 +1,28 @@
 'use strict'
 
 $(function () {
-  $('.btn-outline-success').on('click', (e) => {
+  let herokuApps = ['rokumaru-marcket'];
+
+  $('.btn-outline-success, .card-body').on('click', (e) => {
     // 親要素のcardをクリックすると対象サイトに移動するので親要素のイベントが発動しないようにしておく
     e.stopPropagation();
-    location.href = `https://gosho-techplay.com/`;
   });
 
   $('.card').on('click', (e) => {
     e.preventDefault();
     // 遷移するサイトを決める
-    let pathName = $('.card').attr('name');
-    location.href = `https://gosho-techplay.com/${pathName}`;
+    let pathName = $(e.currentTarget).attr('name')
+    // デプロイ先に合わせたURL設定
+    if (herokuApps.includes(pathName)) {
+      location.href = `https://${pathName}.herokuapp.com`;
+    }
+    else {
+      location.href = `https://gosho-techplay.com/${pathName}`;
+    }
   });
 
   $('.sitetitle').on('click', (e) => {
-    e.preventDefault();
+    e.stopPropagation();
     location.href = `http://localhost:3001/`;
   })
 });
